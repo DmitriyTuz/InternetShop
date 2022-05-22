86dda722b6a6ffbaa591d89cc11fa2fbcfb8aad6
Dmitriy <dmitriytuz123@gmail.com>
2022-05-22 20:57:45 +0300

    refactoring 6

---
+++ server/routes/index.js
@@ -0,0 +1,25 @@
+const Router = require('express')
+const router = new Router()
+const deviceRouter = require('./deviceRouter')
+const userRouter = require('./userRouter')
+const brandRouter = require('./brandRouter')
+const typeRouter = require('./typeRouter')
+const basketRouter = require('./basketRouter')
+const basket_deviceRouter = require('./basket_deviceRouter')
+const ratingRouter = require('./ratingRouter')
+const device_infoRouter = require('./device_infoRouter')
+
+const testRouter = require('./testRouter')
+
+router.use('/user', userRouter)
+router.use('/type', typeRouter)
+router.use('/brand', brandRouter)
+router.use('/device', deviceRouter)
+router.use('/basket', basketRouter)
+router.use('/basket_device', basket_deviceRouter)
+router.use('/rating', ratingRouter)
+router.use('/device_info', device_infoRouter)
+
+router.use('/test', testRouter)
+
+module.exports = router
