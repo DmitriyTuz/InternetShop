38eca10d6838e674938cbff5a3063f71a4c9604b
Dmitriy <dmitriytuz123@gmail.com>
2022-07-06 14:03:20 +0300

    with docker 5

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #npm install
-npx sequelize-cli db:migrate
-#npx browserslist@latest --update-db
+#npx sequelize-cli db:migrate
+npx browserslist@latest --update-db
 #npm start
 #npx sequelize-cli db:migrate:undo
